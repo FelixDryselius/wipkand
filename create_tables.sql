380f72b207fd0827179983dc5409020469a1d163
Johan Blad <johan.blad@hotmail.se>
2018-03-23 17:39:20 +0100

    Updated SQL create tables and added drop tables commands

--- create_tables.sql
+++ create_tables.sql
@@ -1,15 +1,16 @@
 CREATE TABLE product
 (
   article_number char(8) NOT NULL,
+  product_name varchar(255),
   reference_storage INT,
-  label char(8),
+  label varchar(255),
   PRIMARY KEY (article_number)
 );
 
 
 CREATE TABLE product_order
 (
-  order_number INT NOT NULL,
+  order_number char(7) NOT NULL,
   article_number  char(8),
   PRIMARY KEY (order_number),
   FOREIGN KEY (article_number) REFERENCES product(article_number)
@@ -17,7 +18,7 @@ CREATE TABLE product_order
 
 CREATE TABLE batch
 (
-  batch_number INT NOT NULL,
+  batch_number char(10) NOT NULL,
   start_date datetime,
   end_date datetime,
   scrap INT,
@@ -30,7 +31,7 @@ CREATE TABLE batch
   applied_labels INT ,
   rework_time datetime,
   yield_2 INT,
-  order_number INT ,
+  order_number char(7) ,
   PRIMARY KEY (batch_number),
   FOREIGN KEY (order_number) REFERENCES product_order(order_number)
 );
@@ -40,7 +41,7 @@ CREATE TABLE user_comment
   comment_ID int NOT NULL,
   post_date datetime,
   text_comment text ,
-  batch_number INT,
+  batch_number char(10),
   PRIMARY KEY (comment_ID, batch_number),
   FOREIGN KEY (batch_number) REFERENCES batch(batch_number)
 );
---
+++ drop_tables.sql
@@ -0,0 +1,4 @@
+drop table user_comment;
+drop table batch;
+drop table product_order;
+drop table product;
