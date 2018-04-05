203d326affcfcae7f5e2bac3c9e785720f18de1f
Johan Blad <johan.blad@hotmail.se>
2018-04-05 20:12:51 +0200

    Cleanup

--- backend/src/operations/api/urls.py
+++ backend/src/operations/api/urls.py
@@ -15,6 +15,7 @@ from operations.api.views import (
     CommentCreateAPIView,
 )
 
+#TODO: Split comments up to its own app
 urlpatterns = [
     path('product/', ProductListAPIView.as_view(), name='product-list'),
     path('product/<str:pk>/', ProductDetailAPIView.as_view(), name='product-detail'),
@@ -33,10 +34,3 @@ urlpatterns = [
     path('comment/<str:batch_number>/', CommentListAPIView.as_view(), name='batch-comment-list'),
     path('comment/<str:batch_number>/<int:pk>/', CommentDetailAPIView.as_view(), name='comment-detail'),
 ]
-
-    # #API urls
-    # path('operations/api/product', ProductListView.as_view(), name='productAPI'),
-    # path('operations/api/order', OrderListView.as_view(), name='orderAPI'),
-    # path('operations/api/batch', BatchListView.as_view(), name='batchListAPI'),
-    # path('operations/api/batch/<int:pk>', BatchDetailView.as_view(), name='batchDetailAPI'),
-    # path('operations/api/batchComment', BatchCommentListView.as_view(), name='batchCommentAPI'),
