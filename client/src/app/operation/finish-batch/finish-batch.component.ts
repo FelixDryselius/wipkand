099a4f2f0f1aa7a1c4a6a8549af686e03fdf4bd7
Johan Blad <johan.blad@hotmail.se>
2018-05-07 16:10:20 +0200

    Front end validation for finish batch. Cleaned up prodInfo observable so that it either will hold info about a batch, or be null strictly

--- client/src/app/operation/finish-batch/finish-batch.component.ts
+++ client/src/app/operation/finish-batch/finish-batch.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { NgForm, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthAPIService } from '../../auth/auth.service';
-import { OperationsService } from '../shared/services/operations.service';
 import { Batch } from '../../shared/interfaces/batch';
+import { OperationsService } from '../shared/services/operations.service';
 import { QueryResponse } from '../../shared/interfaces/query-response'
 
 // 3rd party imports
@@ -21,34 +21,64 @@ export class FinishBatchComponent implements OnInit {
   reLabeling = "false" //making the radio button "no" checked default 
 
   //the following items are copied from start-batch.component
-  private prodActive: boolean;
+
   private prodInfo: any;
-  private service_prodStatus: any;
   private service_prodInfo: any;
+
+  private finishBatchForm: FormGroup;
 
   constructor(
     private router: Router,
     private operationsService: OperationsService,
     private authAPI: AuthAPIService,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
     //the following items are copied from start-batch.component
-   // this.service_prodStatus = this.operationsService.prodActiveObservable.subscribe(active => this.prodActive = active)
+
     this.service_prodInfo = this.operationsService.prodInfoObservable.subscribe(info => this.prodInfo = info)
+    this.createFinishBatchForm()
+
   }
 
   // TODO: get a correct unsubscribe working
   ngOnDestroy() {
-    // this.service_prodStatus.unsubscribe()
     // this.service_prodInfo.unsubscribe()
+  }
+
+  createFinishBatchForm() {
+    this.finishBatchForm = this.formBuilder.group({
+      'hmi1Good': new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+      ]),
+      'hmi1Bad': new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+      ]),
+      'hmi2Good': new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+      ]),
+      'hmi2Bad': new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+      ]),
+      'yield': new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+      ]),
+      'reLabeling': new FormControl('', [
+        Validators.required,
+      ]),
+    })
   }
 
   submitEndBatch($theEvent, batchForm) {
     // TODO:  add these attributes so the whole batch kan close:
     // scrap, rework_date, applied_labels, label_print_time, rework_time, yield_2
     let batchInfo = {}
-    console.log("submit end runned");
     if (this.prodInfo.active) {
       batchInfo = {
         batch_number: this.prodInfo.batch_number,
@@ -57,17 +87,18 @@ export class FinishBatchComponent implements OnInit {
           article_number: this.prodInfo.article_number,
         },
         end_date: new Date(),
-        production_yield: batchForm.yield_1,
-        hmi1_bad: batchForm.hmi1_bad,
-        hmi2_bad: batchForm.hmi2_bad,
-        hmi1_good: batchForm.hmi1_good,
-        hmi2_good: batchForm.hmi2_good,
+        production_yield: batchForm.yield,
+        hmi1_good: batchForm.hmi1Good,
+        hmi1_bad: batchForm.hmi1Bad,
+
+        hmi2_good: batchForm.hmi2Good,
+        hmi2_bad: batchForm.hmi2Bad,
       }
       console.log(batchInfo)
       this.operationsService.updateBatch(batchInfo as Batch)
         .retryWhen(error => this.authAPI.checkHttpRetry(error))
         .subscribe(data => {
-          this.operationsService.setCurrentBatchInfo(false, null);
+          this.operationsService.setCurrentBatchInfo(null);
           this.router.navigate(['/home'])
         }, error => {
           console.error(error.message)
