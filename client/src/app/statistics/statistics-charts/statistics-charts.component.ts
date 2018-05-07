b4beec7f674b7c3eabc40875309df12b34302116
Felix <felix.dryselius@gmail.com>
2018-05-07 15:13:45 +0200

    temp save

--- client/src/app/statistics/statistics-charts/statistics-charts.component.ts
+++ client/src/app/statistics/statistics-charts/statistics-charts.component.ts
@@ -1,14 +1,13 @@
-// import { Component, OnInit } from '@angular/core';
-
-// import {NgxChartsModule} from '@swimlane/ngx-charts';
-// import { OperationsService } from '../../operation/shared/services/operations.service';
-// import { QueryResponse } from '../../shared/interfaces/query-response';
-// import { Scoreboard } from '../../../assets/interface/scoreboard';
-
-
 import { Component, OnInit } from '@angular/core';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { Observable} from 'rxjs/Rx';
+
+import { AuthAPIService } from '../../auth/auth.service';
+import { OperationsService } from '../../operation/shared/services/operations.service';
+import { QueryResponse } from '../../shared/interfaces/query-response';
+import { Scoreboard } from '../../../assets/interface/scoreboard';
+
+
 
 
 @Component({
@@ -22,7 +21,7 @@ export class StatisticsChartsComponent implements OnInit {
     country: '',
     gender: '',
     rating: 0
-  }
+  };
 
   //data
   chartdata = [
@@ -50,10 +49,109 @@ export class StatisticsChartsComponent implements OnInit {
       "name": "Italy",
       "value": 35800
     }
-  ]
+  ];
 
-  countryCount = [];
-  countryData = [];
+  chartdataLine = [
+    {
+      "name": "Germany",
+      "series": [
+        {
+          "value": 4465,
+          "name": "2016-09-18T11:01:51.638Z"
+        },
+        {
+          "value": 2790,
+          "name": "2016-09-13T07:23:25.764Z"
+        },
+        {
+          "value": 6450,
+          "name": "2016-09-13T14:17:09.128Z"
+        },
+        {
+          "value": 4214,
+          "name": "2016-09-23T23:30:35.974Z"
+        },
+        {
+          "value": 4006,
+          "name": "2016-09-13T02:27:34.421Z"
+        },
+        {
+          "name": "2016-09-12T21:37:39.105Z",
+          "value": 2700
+        },
+        {
+          "name": "2016-09-12T21:11:17.110Z",
+          "value": 5780
+        },
+        {
+          "name": "2016-09-15T11:25:50.248Z",
+          "value": 6538
+        },
+        {
+          "name": "2016-09-14T17:39:46.429Z",
+          "value": 5695
+        },
+        {
+          "name": "2016-09-20T12:09:48.571Z",
+          "value": 6115
+        },
+        {
+          "name": "2016-09-14T11:54:01.596Z",
+          "value": 3695
+        }
+      ]
+    },
+  ​
+    {
+      "name": "USA",
+      "series": [
+        {
+          "value": 6339,
+          "name": "2016-09-18T11:01:51.638Z"
+        },
+        {
+          "value": 5413,
+          "name": "2016-09-13T07:23:25.764Z"
+        },
+        {
+          "value": 5417,
+          "name": "2016-09-13T14:17:09.128Z"
+        },
+        {
+          "value": 4028,
+          "name": "2016-09-23T23:30:35.974Z"
+        },
+        {
+          "value": 4521,
+          "name": "2016-09-13T02:27:34.421Z"
+        },
+        {
+          "name": "2016-09-12T21:37:39.105Z",
+          "value": 4287
+        },
+        {
+          "name": "2016-09-12T21:11:17.110Z",
+          "value": 6092
+        },
+        {
+          "name": "2016-09-15T11:25:50.248Z",
+          "value": 3112
+        },
+        {
+          "name": "2016-09-14T17:39:46.429Z",
+          "value": 6079
+        },
+        {
+          "name": "2016-09-20T12:09:48.571Z",
+          "value": 2168
+        },
+        {
+          "name": "2016-09-14T11:54:01.596Z",
+          "value": 5629
+        }
+      ]
+    }
+  ];
 
   //Chart
   view: any[] = [700, 400];
@@ -66,10 +164,25 @@ export class StatisticsChartsComponent implements OnInit {
   explodeSlices = true;
   doughnut = false;
 
-  constructor() {}
+  constructor(private authAPI:AuthAPIService, private operationsService:OperationsService) { }
+    
+
 
  
   ngOnInit() {
+
+    this.operationsService.getProductionStatistics('?limit=72')
+    .retryWhen(error => this.authAPI.checkHttpRetry(error))
+    .subscribe(data =>{
+      let productionStatistics
+     productionStatistics = (data as QueryResponse).results as Scoreboard []
+     
+     let productionQuantity = productionStatistics.map(res => res.production_quantity)
+     let productionTimeStamp = productionStatistics.map(res => new Date(res.time_stamp).toLocaleTimeString('sv-SV', { year: 'numeric', month: 'numeric', day: 'numeric', hour:'numeric', minute:'numeric'}) )
+ 
+     console.log('productionQuantity: ' + productionQuantity);
+     console.log('productionTimeStamp: ' + productionTimeStamp);
+    })    
   }
 
   pressed(event) {
@@ -88,3 +201,59 @@ export class StatisticsChartsComponent implements OnInit {
     ] 
   }
 }
+
+
+
+// import { Component, OnInit } from '@angular/core';
+
+// import { Chart } from 'chart.js';
+// import { OperationsService } from '../../operation/shared/services/operations.service';
+// import { QueryResponse } from '../../shared/interfaces/query-response';
+// import { Scoreboard } from '../../../assets/interface/scoreboard';
+
+// @Component({
+//   selector: 'app-statistics-charts',
+//   templateUrl: './statistics-charts.component.html',
+//   styleUrls: ['./statistics-charts.component.css']
+// })
+// export class StatisticsChartsComponent implements OnInit {
+//   chart =[];
+//   constructor(private operationsService: OperationsService) { }
+
+//   ngOnInit() {
+  
+
+
+
+    
+//     this.chart = new Chart('canvas', {
+//       type: 'line',
+//       data: {
+//         labels: productionTimeStamp,
+//         datasets: [
+//           { 
+//             data: productionQuantity,
+//             borderColor: "#3cba9f",
+//             fill: false
+//           },
+//         ]
+//       },
+//       options: {
+//         legend: {
+//           display: false
+//         },
+//         scales: {
+//           xAxes: [{
+//             display: true
+//           }],
+//           yAxes: [{
+//             display: true
+//           }],
+//         }
+//       }
+//     });
+    
+    
+//    }) 
+//   }
+// }
