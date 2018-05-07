44be5deb0f12aff0a23d19eabbc8efb044f1e34a
Felix <felix.dryselius@gmail.com>
2018-05-07 19:17:14 +0200

    Now we have functioning charts. Statistics/charts is updated and also package.json, with comments. It should work fine!

--- SQL/run_all.sql
+++ SQL/run_all.sql
@@ -253,9 +253,6 @@ INSERT INTO production_statistic (batch_number, time_stamp, production_quantity,
 VALUES ('1000000001', '2018-03-01 11:00:00', 500, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000001', '2018-03-01 12:00:00', 1200, 3);
-
-INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
 VALUES ('1000000001', '2018-03-01 13:00:00', 2000, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
@@ -322,40 +319,40 @@ INSERT INTO production_statistic (batch_number, time_stamp, production_quantity,
 VALUES ('1000000003', '2018-03-02 10:00:00', 0, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000004', '2018-03-03 11:00:00', 500, 3);
+VALUES ('1000000004', '2018-03-02 11:00:00', 500, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000004', '2018-03-03 13:00:00', 2000, 3);
+VALUES ('1000000004', '2018-03-02 13:00:00', 2000, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000004', '2018-03-03 14:00:00', 2100, 3);
+VALUES ('1000000004', '2018-03-02 14:00:00', 2100, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000004', '2018-03-03 15:00:00', 2000, 3);
+VALUES ('1000000004', '2018-03-02 15:00:00', 2000, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000004', '2018-03-03 16:00:00', 2000, 3);
+VALUES ('1000000004', '2018-03-02 16:00:00', 2000, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000004', '2018-03-03 17:00:00', 2000, 3);
+VALUES ('1000000004', '2018-03-02 17:00:00', 2000, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000004', '2018-03-03 18:00:00', 2000, 3);
+VALUES ('1000000004', '2018-03-02 18:00:00', 2000, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000004', '2018-03-03 19:00:00', 2000, 3);
+VALUES ('1000000004', '2018-03-02 19:00:00', 2000, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000004', '2018-03-03 20:00:00', 2000, 3);
+VALUES ('1000000004', '2018-03-02 20:00:00', 2000, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000004', '2018-03-03 21:00:00', 2000, 3);
+VALUES ('1000000004', '2018-03-02 21:00:00', 2000, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000004', '2018-03-03 22:00:00', 2000, 3);
+VALUES ('1000000004', '2018-03-02 22:00:00', 2000, 3);
 
 INSERT INTO production_statistic (batch_number, time_stamp, production_quantity, staff_quantity)
-VALUES ('1000000004', '2018-03-03 23:00:00', 2000, 3);
+VALUES ('1000000004', '2018-03-02 23:00:00', 2000, 3);
 
 
 INSERT INTO floorstock_statistic (floorstock_item_id, time_stamp, quantity, batch_number)
--- client/src/app/statistics/statistics-charts/statistics-charts.component.ts
+++ client/src/app/statistics/statistics-charts/statistics-charts.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { Observable} from 'rxjs/Rx';
 
+//3rd party and application imports
 import { AuthAPIService } from '../../auth/auth.service';
+import { element } from 'protractor';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { OperationsService } from '../../operation/shared/services/operations.service';
 import { QueryResponse } from '../../shared/interfaces/query-response';
 import { Scoreboard } from '../../../assets/interface/scoreboard';
-
-
 
 
 @Component({
@@ -17,243 +16,120 @@ import { Scoreboard } from '../../../assets/interface/scoreboard';
 })
 export class StatisticsChartsComponent implements OnInit {
   
-  survey = {
-    country: '',
-    gender: '',
-    rating: 0
-  };
-
   //data
-  chartdata = [
-    {
-      "name": "Germany",
-      "value": 40632
-    },
-    {
-      "name": "United States",
-      "value": 49737
-    },
-    {
-      "name": "France",
-      "value": 36745
-    },
-    {
-      "name": "United Kingdom",
-      "value": 36240
-    },
-    {
-      "name": "Spain",
-      "value": 33000
-    },
-    {
-      "name": "Italy",
-      "value": 35800
-    }
-  ];
+  haveData=false;
+  displayData;
+  showBatches = false;
+  prodDataSeparateBatches = [];
+  prodDataContinues = [];
 
-  chartdataLine = [
-    {
-      "name": "Germany",
-      "series": [
-        {
-          "value": 4465,
-          "name": "2016-09-18T11:01:51.638Z"
-        },
-        {
-          "value": 2790,
-          "name": "2016-09-13T07:23:25.764Z"
-        },
-        {
-          "value": 6450,
-          "name": "2016-09-13T14:17:09.128Z"
-        },
-        {
-          "value": 4214,
-          "name": "2016-09-23T23:30:35.974Z"
-        },
-        {
-          "value": 4006,
-          "name": "2016-09-13T02:27:34.421Z"
-        },
-        {
-          "name": "2016-09-12T21:37:39.105Z",
-          "value": 2700
-        },
-        {
-          "name": "2016-09-12T21:11:17.110Z",
-          "value": 5780
-        },
-        {
-          "name": "2016-09-15T11:25:50.248Z",
-          "value": 6538
-        },
-        {
-          "name": "2016-09-14T17:39:46.429Z",
-          "value": 5695
-        },
-        {
-          "name": "2016-09-20T12:09:48.571Z",
-          "value": 6115
-        },
-        {
-          "name": "2016-09-14T11:54:01.596Z",
-          "value": 3695
-        }
-      ]
-    },
-  ​
-    {
-      "name": "USA",
-      "series": [
-        {
-          "value": 6339,
-          "name": "2016-09-18T11:01:51.638Z"
-        },
-        {
-          "value": 5413,
-          "name": "2016-09-13T07:23:25.764Z"
-        },
-        {
-          "value": 5417,
-          "name": "2016-09-13T14:17:09.128Z"
-        },
-        {
-          "value": 4028,
-          "name": "2016-09-23T23:30:35.974Z"
-        },
-        {
-          "value": 4521,
-          "name": "2016-09-13T02:27:34.421Z"
-        },
-        {
-          "name": "2016-09-12T21:37:39.105Z",
-          "value": 4287
-        },
-        {
-          "name": "2016-09-12T21:11:17.110Z",
-          "value": 6092
-        },
-        {
-          "name": "2016-09-15T11:25:50.248Z",
-          "value": 3112
-        },
-        {
-          "name": "2016-09-14T17:39:46.429Z",
-          "value": 6079
-        },
-        {
-          "name": "2016-09-20T12:09:48.571Z",
-          "value": 2168
-        },
-        {
-          "name": "2016-09-14T11:54:01.596Z",
-          "value": 5629
-        }
-      ]
-    }
-  ];
-
-  //Chart
-  view: any[] = [700, 400];
+  //Chart here we set options fot the chart
   showLegend = true;
-
   colorScheme = {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
   };
   showLabels = true;
-  explodeSlices = true;
-  doughnut = false;
+
 
   constructor(private authAPI:AuthAPIService, private operationsService:OperationsService) { }
-    
 
-
- 
   ngOnInit() {
+    this.getProductionData()           
+  }
 
-    this.operationsService.getProductionStatistics('?limit=72')
+  xAxisFormatting(data){
+    return data.toLocaleTimeString('sv-SV', { year: 'numeric', month: 'numeric', day: 'numeric', hour:'numeric', minute:'numeric'})
+  }
+
+  //Might be used later
+  pressed(event) {
+    console.log('event triggered');   
+  }
+
+  changeTimeSpan(query?:string){
+    let tempQuery = '?limit='+query;
+    this.getProductionData(tempQuery)
+  }
+  
+  toggleBatches(toggle:boolean){
+    console.log('toggleBatches: '+toggle);
+    this.showBatches = toggle
+    if(toggle){
+      this.displayData=this.prodDataSeparateBatches
+    } else {
+      this.displayData=this.prodDataContinues
+    }
+  }
+
+  getProductionData(query = '?limit=72')  {
+
+    this.operationsService.getProductionStatistics(query)
     .retryWhen(error => this.authAPI.checkHttpRetry(error))
     .subscribe(data =>{
       let productionStatistics
-     productionStatistics = (data as QueryResponse).results as Scoreboard []
-     
-     let productionQuantity = productionStatistics.map(res => res.production_quantity)
-     let productionTimeStamp = productionStatistics.map(res => new Date(res.time_stamp).toLocaleTimeString('sv-SV', { year: 'numeric', month: 'numeric', day: 'numeric', hour:'numeric', minute:'numeric'}) )
- 
-     console.log('productionQuantity: ' + productionQuantity);
-     console.log('productionTimeStamp: ' + productionTimeStamp);
-    })    
-  }
-
-  pressed(event) {
-    console.log('event triggered');
-    
-    console.log(event);
-    this.chartdata = [
-      {
-        "name": "Germany",
-        "value": 8940000
-      },
-      {
-        "name": "USA",
-        "value": 5000000
-      }
-    ] 
+      productionStatistics = (data as QueryResponse).results as Scoreboard []
+      
+      // Creating an to match what ngx-charts need to have
+      // the element looks like this:
+      //      chartdataLine =[
+      //        {
+      //          'name':batch_number          (this is unique )
+      //          'series': [
+      //            {
+      //              'name':batch_number,
+      //              'value:' production_quantity
+      //            }
+      //          ]
+      //        }
+      //      ] 
+      let relevantBatches = []
+      let productionDataPoints = []
+      let arrayHolder = []
+      let prodDataContinuesTemp = []
+      productionStatistics.forEach(element => {
+        if(relevantBatches.indexOf(element.batch_number) == -1 ){
+          relevantBatches.push(element.batch_number)                 
+          productionDataPoints.push({
+            'name':element.batch_number,
+            'series':[
+              {
+                'value':element.production_quantity,
+                'name': new Date(element.time_stamp)
+              }
+           ]
+          })
+        } else {
+          productionDataPoints.forEach(subEl =>{            
+            if(subEl.name==element.batch_number){             
+              subEl.series.push({
+              'value':element.production_quantity,
+              'name': new Date(element.time_stamp)
+              })
+            }
+          })
+        }
+        // Here it saves the 'continues production run'      
+        arrayHolder.push(
+          {
+            'name':new Date(element.time_stamp),
+            'value': element.production_quantity
+          }
+        )         
+      });
+      this.prodDataSeparateBatches = productionDataPoints;
+      this.prodDataContinues = [
+        {
+          'name':'Production Quantity',
+          'series': arrayHolder
+        }
+      ]
+    //sets which type to show
+    if(this.showBatches){
+      this.displayData = this.prodDataSeparateBatches
+    } else {
+      this.displayData = this.prodDataContinues
+    }
+    this.haveData=true
+    })
   }
 }
-
-
-
-// import { Component, OnInit } from '@angular/core';
-
-// import { Chart } from 'chart.js';
-// import { OperationsService } from '../../operation/shared/services/operations.service';
-// import { QueryResponse } from '../../shared/interfaces/query-response';
-// import { Scoreboard } from '../../../assets/interface/scoreboard';
-
-// @Component({
-//   selector: 'app-statistics-charts',
-//   templateUrl: './statistics-charts.component.html',
-//   styleUrls: ['./statistics-charts.component.css']
-// })
-// export class StatisticsChartsComponent implements OnInit {
-//   chart =[];
-//   constructor(private operationsService: OperationsService) { }
-
-//   ngOnInit() {
-  
-
-
-
-    
-//     this.chart = new Chart('canvas', {
-//       type: 'line',
-//       data: {
-//         labels: productionTimeStamp,
-//         datasets: [
-//           { 
-//             data: productionQuantity,
-//             borderColor: "#3cba9f",
-//             fill: false
-//           },
-//         ]
-//       },
-//       options: {
-//         legend: {
-//           display: false
-//         },
-//         scales: {
-//           xAxes: [{
-//             display: true
-//           }],
-//           yAxes: [{
-//             display: true
-//           }],
-//         }
-//       }
-//     });
-    
-    
-//    }) 
-//   }
-// }
