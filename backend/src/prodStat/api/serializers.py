af83d25cc58dc1ed599da9b047d7854051c633c6
Johan Blad <johan.blad@hotmail.se>
2018-05-11 11:10:23 +0200

    Added extended batch info to serializers for production statistics and floorstock statistics for GET requests

--- backend/src/prodStat/api/serializers.py
+++ backend/src/prodStat/api/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework.serializers import ModelSerializer
 
 from prodStat.models import ProductionStatistic
+from operations.api.serializers import BatchInfoSerializer
 
 class ProductionStatisticSerializer(ModelSerializer):
 
     class Meta:
         model = ProductionStatistic
         fields = '__all__'
+
+class ProductionStatisticExtendedSerializer(ProductionStatisticSerializer):
+    batch = BatchInfoSerializer()
+
+    class Meta:
+        model = ProductionStatistic
+        fields = '__all__'
+        read_only_fields = ['time_stamp']
 
 class ProductionStatisticSerializerReadOnlyDate(ProductionStatisticSerializer):
 
