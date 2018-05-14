accbba716dd94cad0ece2c43fe89b71e8dddb229
FelixDryselius <FelixDryselius@users.noreply.github.com>
2018-05-14 16:36:43 +0200

    removed unused charts pppmh and staff-quant

--- client/src/app/statistics/statistics.module.ts
+++ client/src/app/statistics/statistics.module.ts
@@ -11,9 +11,7 @@ import { StatisticsRoutingModule } from './statistics-routing.module';
 //big test
 import { ComboChartComponent } from './charts/combo-chart/combo-chart.component';
 import { ComboSeriesVerticalComponent } from './charts/combo-chart/combo-series-vertical.component';
-import { StaffQuantityComponent } from './charts/staff-quantity/staff-quantity.component';
 import { BatchChartComponent } from './charts/batch/batch-chart.component';
-import { ProducedPerPersonComponent } from './charts/produced-per-person/produced-per-person.component';
 import { ProductionPerTimeUnitComponent } from './charts/production-per-time-unit/production-per-time-unit.component';
 
 
@@ -31,9 +29,7 @@ import { ProductionPerTimeUnitComponent } from './charts/production-per-time-uni
     ComboSeriesVerticalComponent, 
     DataPageComponent,
     StatisticsChartsComponent,
-    StaffQuantityComponent,
     BatchChartComponent,
-    ProducedPerPersonComponent,
     ProductionPerTimeUnitComponent],
 
   providers: [],
