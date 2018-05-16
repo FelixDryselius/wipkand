6bc54ec43b0fb37b853528ab78a27fbfe1f782c1
Johan Blad <johan.blad@hotmail.se>
2018-05-16 12:47:16 +0200

    Added functions for pick and replace and applied labels in batch rework. Cleanup in finish batch and connected it to rework

--- client/src/app/operation/batch-rework/batch-rework.component.ts
+++ client/src/app/operation/batch-rework/batch-rework.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { FinishBatchComponent } from '../finish-batch/finish-batch.component';
+import { Batch } from '../../shared/interfaces/batch';
 
 @Component({
   selector: 'batch-rework',
@@ -13,25 +14,37 @@ export class BatchReworkComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    if (this.reworkForm.controls.hmi1Bad_rework) {
-      this.reworkForm.controls.hmi1Bad_rework.enable()
-      this.reworkForm.controls.hmi2Bad_rework.enable()
-      this.reworkForm.controls.hmi1Good_rework.enable()
-      this.reworkForm.controls.hmi2Good_rework.enable()
+    if (this.reworkForm.controls.hmi1_total_rework) {
+      this.reworkForm.controls.hmi1_total_rework.enable()
+      this.reworkForm.controls.hmi1_good_rework.enable()
+      this.reworkForm.controls.hmi1_bad_rework.enable()
+
+      this.reworkForm.controls.hmi2_total_rework.enable()
+      this.reworkForm.controls.hmi2_good_rework.enable()
+      this.reworkForm.controls.hmi2_bad_rework.enable()
     } else {
-      this.reworkForm.addControl('hmi1Good_rework', new FormControl('', [
+      this.reworkForm.addControl('hmi1_total_rework', new FormControl('', [
         Validators.required,
         Validators.pattern("^[0-9]*$"),
       ]))
-      this.reworkForm.addControl('hmi1Bad_rework', new FormControl('', [
+      this.reworkForm.addControl('hmi1_good_rework', new FormControl('', [
         Validators.required,
         Validators.pattern("^[0-9]*$"),
       ]))
-      this.reworkForm.addControl('hmi2Good_rework', new FormControl('', [
+      this.reworkForm.addControl('hmi1_bad_rework', new FormControl('', [
         Validators.required,
         Validators.pattern("^[0-9]*$"),
       ]))
-      this.reworkForm.addControl('hmi2Bad_rework', new FormControl('', [
+
+      this.reworkForm.addControl('hmi2_total_rework', new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+      ]))
+      this.reworkForm.addControl('hmi2_good_rework', new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+      ]))
+      this.reworkForm.addControl('hmi2_bad_rework', new FormControl('', [
         Validators.required,
         Validators.pattern("^[0-9]*$"),
       ]))
@@ -39,12 +52,31 @@ export class BatchReworkComponent implements OnInit {
   }
 
   ngOnDestroy(): void {
-    if (this.reworkForm.controls.hmi1Bad_rework) {
-      this.reworkForm.controls.hmi1Bad_rework.disable()
-      this.reworkForm.controls.hmi2Bad_rework.disable()
-      this.reworkForm.controls.hmi1Good_rework.disable()
-      this.reworkForm.controls.hmi2Good_rework.disable()
+    if (this.reworkForm.controls.hmi1_total_rework) {
+      this.reworkForm.controls.hmi1_total_rework.disable()
+      this.reworkForm.controls.hmi1_good_rework.disable()
+      this.reworkForm.controls.hmi1_bad_rework.disable()
+
+      this.reworkForm.controls.hmi2_total_rework.disable()
+      this.reworkForm.controls.hmi2_good_rework.disable()
+      this.reworkForm.controls.hmi2_bad_rework.disable()
     }
   }
 
+  test() {
+    console.log("It Works!");
+  }
+
+  getAppliedLabels() {
+    return this.reworkForm.controls.hmi1_total_rework.value + this.reworkForm.controls.hmi2_total_rework.value
+  }
+
+  getPickAndReplace(form: Batch, applied_labels: number, scrap: number): number {
+    if (applied_labels) {
+      return ((form.hmi1_bad + form.hmi2_bad)
+        - applied_labels
+        - (scrap * 10))
+    }
+    return 0
+  }
 }
--- client/src/app/operation/finish-batch/finish-batch.component.ts
+++ client/src/app/operation/finish-batch/finish-batch.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -6,6 +6,7 @@ import { AuthAPIService } from '../../auth/auth.service';
 import { Batch } from '../../shared/interfaces/batch';
 import { OperationsService } from '../shared/services/operations.service';
 import { QueryResponse } from '../../shared/interfaces/query-response'
+import { BatchReworkComponent } from '../batch-rework/batch-rework.component';
 
 // 3rd party imports
 
@@ -16,10 +17,14 @@ import { QueryResponse } from '../../shared/interfaces/query-response'
 })
 export class FinishBatchComponent implements OnInit {
 
+  @ViewChild(BatchReworkComponent) batchReworkComponent: BatchReworkComponent
+
   private prodInfo: any;
   private service_prodInfoSub: any;
   private createBatchSub: any;
+  private getProductSub: any;
 
+  private currentProduct: any;
   private finishBatchForm: FormGroup;
 
   constructor(
@@ -31,6 +36,15 @@ export class FinishBatchComponent implements OnInit {
 
   ngOnInit() {
     this.service_prodInfoSub = this.operationsService.$prodInfo.subscribe(info => this.prodInfo = info)
+    if (this.prodInfo) {
+      console.log("Fetchng product");
+      this.getProductSub = this.operationsService.getProduct(this.prodInfo.order.article_number)
+        .retryWhen(error => this.authAPI.checkHttpRetry(error))
+        .subscribe(data => {
+          this.currentProduct = data
+        })
+    }
+
     this.createFinishBatchForm()
   }
 
@@ -39,25 +53,39 @@ export class FinishBatchComponent implements OnInit {
     this.service_prodInfoSub.unsubscribe()
   }
 
+  // fbTest() {
+  //   this.batchReworkComponent.calculate()
+  // }
+
+  calculateScrap(form: Batch, refLagerQuantity: number): number {
+    let scrap = Math.ceil(((
+      (form.hmi1_good + form.hmi1_bad + form.hmi2_good + form.hmi2_bad)
+      - (form.production_yield * 10)) / 10)
+      - refLagerQuantity
+    )
+    return scrap
+    //return scrap > 0 ? scrap: 0
+  }
+
   createFinishBatchForm() {
     this.finishBatchForm = this.formBuilder.group({
-      'hmi1Good': new FormControl('', [
+      'hmi1_good': new FormControl('', [
         Validators.required,
         Validators.pattern("^[0-9]*$"),
       ]),
-      'hmi1Bad': new FormControl('', [
+      'hmi1_bad': new FormControl('', [
         Validators.required,
         Validators.pattern("^[0-9]*$"),
       ]),
-      'hmi2Good': new FormControl('', [
+      'hmi2_good': new FormControl('', [
         Validators.required,
         Validators.pattern("^[0-9]*$"),
       ]),
-      'hmi2Bad': new FormControl('', [
+      'hmi2_bad': new FormControl('', [
         Validators.required,
         Validators.pattern("^[0-9]*$"),
       ]),
-      'yield': new FormControl('', [
+      'production_yield': new FormControl('', [
         Validators.required,
         Validators.pattern("^[0-9]*$"),
       ]),
@@ -67,35 +95,33 @@ export class FinishBatchComponent implements OnInit {
     })
   }
 
-  submitEndBatch($theEvent, batchForm) {
-    // TODO:  add these attributes so the whole batch kan close:
-    // scrap, rework_date, applied_labels, label_print_time, rework_time, yield_2
-    let batchInfo = {}
-    if (this.prodInfo) {
-      batchInfo = {
-        id: this.prodInfo.id,
-        batch_number: this.prodInfo.batch_number,
-        order: this.prodInfo.order,
-        end_date: new Date(),
-        production_yield: batchForm.yield,
-        hmi1_good: batchForm.hmi1Good,
-        hmi1_bad: batchForm.hmi1Bad,
-        hmi2_good: batchForm.hmi2Good,
-        hmi2_bad: batchForm.hmi2Bad,
-      }
-      console.log(batchInfo)
-      this.createBatchSub = this.operationsService.updateBatch(batchInfo as Batch)
-        .retryWhen(error => this.authAPI.checkHttpRetry(error))
-        .subscribe(data => {
-          this.operationsService.setCurrentBatchInfo(null);
-          this.createBatchSub.unsubscribe()
-          this.router.navigate([''])
-
-        }
-          // , error => {
-          //   console.error(error.message)
-          // }
-        )
+  submitEndBatch($theEvent, form) {
+    let batch: Batch = {
+      id: this.prodInfo.id,
+      batch_number: this.prodInfo.batch_number,
+      order: this.prodInfo.order,
+      end_date: new Date(),
+      scrap: this.calculateScrap(form, this.currentProduct.reference_storage),
+      production_yield: form.production_yield,
+      hmi1_good: form.hmi1Good,
+      hmi1_bad: form.hmi1Bad,
+      hmi2_good: form.hmi2Good,
+      hmi2_bad: form.hmi2Bad,
     }
+
+    if (form.rework == 'true') {
+      //let _applied_labels = this.batchReworkComponent.getAppliedLabels()
+      batch.applied_labels = this.batchReworkComponent.getAppliedLabels()
+      let _pick_and_replace = this.batchReworkComponent.getPickAndReplace(form, batch.applied_labels, batch.scrap)
+    }
+
+    this.createBatchSub = this.operationsService.updateBatch(batch)
+      .retryWhen(error => this.authAPI.checkHttpRetry(error))
+      .subscribe(data => {
+        this.operationsService.setCurrentBatchInfo(null);
+        this.createBatchSub.unsubscribe()
+        this.router.navigate([''])
+      }
+      )
   }
 }
